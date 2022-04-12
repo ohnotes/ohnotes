345b74651a66210ff44b77a4419123f0e190371d
z3oxs <z3ox1s@protonmail.com>
2022-04-12 03:05:47 -0300

    Fixes

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,7 @@ import Notes from './pages/Notes';
 import NotFound from './pages/404';
 import Forbidden from './pages/403';
 
-//defaults.baseURL = 'https://api-ohnotes.herokuapp.com';
-defaults.baseURL = 'http://192.168.0.102:3001';
+defaults.baseURL = 'https://api-ohnotes.herokuapp.com';
 defaults.headers.common.Authorization = `Bearer ${ localStorage.getItem("token") }`;
 defaults.headers.post['Content-Type'] = 'application/json';
 
